0b3263534b988338dce2f41cd1fc383a71fa57eb
hmkim199 <hmkim199@gmail.com>
2022-07-22 17:17:37 +0900

    feat: 의존 관계 주입 - 일반 메소드 주입

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -29,16 +29,4 @@ public class OrderServiceTest {
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
 
     }
-
-//    @Test
-//    void fieldInjectionTest() {
-//        // 의존성 주입 - 필드 인젝션 하면 수정할 수 없으므로 순수 자바 쓰는 테스트 만들려면 이런 식으로 setter를 다시 만들어줘야함....
-//        // 그럴 거면 수정자 주입을 쓰겠지~! 이도 저도 아니라서 쓰지 말자.
-//        OrderServiceImpl orderService = new OrderServiceImpl();
-//
-////        orderService.setMemberRepository(new MemoryMemberRepository());
-////        orderService.setDiscountPolicy(new FixDiscountPolicy());
-//
-//        orderService.createOrder(1L, "itemA", 10000);
-//    }
 }
