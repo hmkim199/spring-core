81c8138816e2ab18227ad2c076f9aa5d24bfe510
hmkim199 <hmkim199@gmail.com>
2022-08-11 17:32:56 +0900

    feat: proxy 이용 request scope bean 생성 지연

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,10 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") // 이 빈은 HTTP 요청 당 하나씩 생성, HTTP 요청이 끝나는 시점에 소멸.
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) // 대상이 클래스면 TARGET_CLASS, 인터페이스면 INTERFACE 하면 됨.
+// 이렇게 하면 MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다.
+// 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청하는 위임 로직이 들어있다.
+
 public class MyLogger {
     private String uuid;
     private String requestURL;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,17 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // MyLogger 대신 MyLogger를 찾을 수 있는(DL 할 수 있는) provider가 주입!!!
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
-//        Thread.sleep(1000);
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
     }
