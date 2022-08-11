3cd6b36473860b46d808982c0f3b77c19054d4b1
hmkim199 <hmkim199@gmail.com>
2022-08-11 16:52:14 +0900

    feat: request scope 오류

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger; // 스프링 컨테이너 뜨는 시점에 의존 관계 주입할텐데 얘는 request 스코프이기 때문에 고객 요청 아직 없으니 오류 발생!
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
